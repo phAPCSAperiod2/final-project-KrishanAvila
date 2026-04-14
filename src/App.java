6f3f7c17b76557d1ad87064ae5a93cb9952bd57c
KrishanAvila <krishavi061308@gmail.com>
2026-04-14 17:00:44 +0000

    added boolean and simple fixes

--- src/App.java
+++ src/App.java
@@ -1,33 +1,42 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean running = true;
 
         System.out.println("Sleep Tracker App");
 
-        System.out.print("Enter the hour you went to sleep (0-23): ");
-        int sleepTime = input.nextInt();
+        while (running) {
+            System.out.println("\n1. Enter sleep data");
+            System.out.println("2. Exit");
+            System.out.print("Choose an option: ");
+            int choice = input.nextInt();
 
-        System.out.print("Enter the hour you woke up (0-23): ");
-        int wakeTime = input.nextInt();
+            if (choice == 1) {
 
-        int hoursSlept;
+                System.out.print("Enter sleep time (0-23): ");
+                int sleepTime = input.nextInt();
 
-        if (wakeTime >= sleepTime) {
-            hoursSlept = wakeTime - sleepTime;
-        } else {
-            hoursSlept = (24 - sleepTime) + wakeTime;
-        }
+                System.out.print("Enter wake time (0-23): ");
+                int wakeTime = input.nextInt();
 
-        System.out.println("You slept for " + hoursSlept + " hours.");
+                int hoursSlept;
 
-        if (hoursSlept < 7) {
-            System.out.println("You should try to get more sleep.");
-        } else if (hoursSlept <= 9) {
-            System.out.println("Good amount of sleep.");
-        } else {
-            System.out.println("You might be oversleeping.");
+                if (wakeTime >= sleepTime) {
+                    hoursSlept = wakeTime - sleepTime;
+                } else {
+                    hoursSlept = (24 - sleepTime) + wakeTime;
+                }
+
+                System.out.println("You slept for " + hoursSlept + " hours.");
+
+            } else if (choice == 2) {
+                running = false;
+                System.out.println("Goodbye!");
+            } else {
+                System.out.println("Invalid choice.");
+            }
         }
 
         input.close();
