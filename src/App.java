edfbbbf9ad2c45c2a554dcb432cd573c089741f5
KrishanAvila <krishavi061308@gmail.com>
2026-04-23 17:10:36 +0000

    now stores up to 7 days of data for the user.

--- src/App.java
+++ src/App.java
@@ -5,15 +5,28 @@ public class App {
         Scanner input = new Scanner(System.in);
         boolean running = true;
 
+        // Arrays to store up to 7 days
+        int[] sleepTimes = new int[7];
+        int[] wakeTimes = new int[7];
+        int[] hoursSleptList = new int[7];
+        int[] restRatings = new int[7];
+        int count = 0;
+
         System.out.println("Sleep Tracker App");
 
         while (running) {
             System.out.println("\n1. Enter sleep data");
-            System.out.println("2. Exit");
+            System.out.println("2. View sleep history");
+            System.out.println("3. Exit");
             System.out.print("Choose an option: ");
             int choice = input.nextInt();
 
             if (choice == 1) {
+
+                if (count >= 7) {
+                    System.out.println("Storage full. Max 7 days.");
+                    continue;
+                }
 
                 System.out.print("Enter sleep time (0-23): ");
                 int sleepTime = input.nextInt();
@@ -32,21 +45,40 @@ public class App {
                 System.out.print("How rested do you feel? (1-5): ");
                 int restRating = input.nextInt();
 
+                // Store data
+                sleepTimes[count] = sleepTime;
+                wakeTimes[count] = wakeTime;
+                hoursSleptList[count] = hoursSlept;
+                restRatings[count] = restRating;
+                count++;
+
                 System.out.println("\n--- Sleep Summary ---");
                 System.out.println("Sleep time: " + sleepTime);
                 System.out.println("Wake time: " + wakeTime);
                 System.out.println("Hours slept: " + hoursSlept);
                 System.out.println("Rest rating: " + restRating + "/5");
 
-                if (restRating <= 2) {
-                    System.out.println("You are not well rested.");
-                } else if (restRating == 3) {
-                    System.out.println("You are somewhat rested.");
+            } else if (choice == 2) {
+
+                if (count == 0) {
+                    System.out.println("No data yet.");
                 } else {
-                    System.out.println("You feel well rested.");
+                    int total = 0;
+
+                    for (int i = 0; i < count; i++) {
+                        System.out.println("\nDay " + (i + 1));
+                        System.out.println("Sleep: " + sleepTimes[i]);
+                        System.out.println("Wake: " + wakeTimes[i]);
+                        System.out.println("Hours: " + hoursSleptList[i]);
+                        System.out.println("Rest: " + restRatings[i] + "/5");
+
+                        total += hoursSleptList[i];
+                    }
+
+                    System.out.println("\nAverage sleep: " + (total / count) + " hours");
                 }
 
-            } else if (choice == 2) {
+            } else if (choice == 3) {
                 running = false;
                 System.out.println("Goodbye!");
             } else {
